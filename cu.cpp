2eb59665db280b4fe2e9ca72e9fb4b7dd34e4c8a
david233421 <aald080507hgtlpva4@cbtis217.edu.mx>
2024-10-15 16:08:39 +0200

    cuadratica

---
+++ cu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+int main() {
+    double a,b,c;
+    double x1,x2;
+    int p;
+    cout<<"ingrese el valor de a: "<<endl;
+    cin>>a;
+    cout<<"ingrese el valor de b: "<<endl;
+    cin>>b;
+    cout<<"ingrese el valor de c: "<<endl;
+    cin>>c;
+
+    p=sqrt(pow(b,2)-4*a*c);
+
+
+    x1=(-b+p)/(2*a);
+    x2=(-b-p)/(2*a);
+    cout<<"el valor es: "<<x1<<endl;
+    cout<<"el valor es: "<<x2<<endl;
+  return 0;
+}
